db91afb48236ac7cac31e64f2666ec94508f4d13
JoãoFelipeLautert <joao.lautert@outlook.com>
2026-03-24 22:07:30 -0300

    chore: update TypeScript configuration and add Metro bundler alias
    - Modified tsconfig.json to set baseUrl and adjust paths for module resolution.
    - Included metro.config.js to define an alias for the project root using Expo's default configuration.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Alert, ScrollView, ActivityIndicator } from 'react-native';
 import { useAuth } from '@/context/AuthContext';
 import { supabase } from '@/supabaseConfig';
-import { useRouter } from 'expo-router';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useRouter } from 'expo-router';
+import React, { useCallback, useEffect, useState } from 'react';
+import { ActivityIndicator, Alert, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 interface Produto {
   id: string;
